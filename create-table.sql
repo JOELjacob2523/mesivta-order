ff6b1b8460f9aee09fe12b5c51d7c34c81679724
JOELjacob2523 <Shlomejacob@gmail.com>
2024-01-02 15:30:14 -0500

    add css & touch ups

--- create-table.sql
+++ create-table.sql
@@ -18,9 +18,9 @@ CREATE TABLE users (
 
 CREATE TABLE vendors(
   vendorId BIGINT IDENTITY (1,1) PRIMARY KEY,
-  username VARCHAR (255) NULL,
+  vendorname VARCHAR (255) NULL,
   companyname VARCHAR (255) NULL,
-  email VARCHAR (255) NULL,
+  vendoremail VARCHAR (255) NULL,
   phone VARCHAR (255) NULL,
   token VARCHAR (MAX) NULL
 );
@@ -50,8 +50,8 @@ CREATE TABLE orderitems(
 /* This is for MySQL*/
 CREATE TABLE users (
   userId BIGINT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NULL,
   username VARCHAR(255) NULL,
+  email VARCHAR(255) NULL,
   password VARCHAR(255) NULL,
   building VARCHAR(10) NULL,
   token TEXT NULL,
@@ -61,9 +61,39 @@ CREATE TABLE users (
 CREATE TABLE products (
   productId BIGINT NOT NULL AUTO_INCREMENT,
   description VARCHAR(255),
+  perCase VARCHAR (255) NULL,
   price VARCHAR (255) NULL,
-  userId BIGINT,
-  FOREIGN KEY(userId) REFERENCES users(userId),
+  FOREIGN KEY(vendorId) REFERENCES vendors(vendorId),
   PRIMARY KEY (productId)
+);
+
+CREATE TABLE vendors (
+  vendorId BIGINT NOT NULL AUTO_INCREMENT,
+  vendorname VARCHAR (255) NULL,
+  companyname VARCHAR (255) NULL,
+  vendoremail VARCHAR (255) NULL,
+  phone VARCHAR (255) NULL,
+  token TEXT NULL
+);
+
+CREATE TABLE orders (
+  orderId BIGINT NOT NULL AUTO_INCREMENT,
+  date DATE NULL,
+  totalboxes VARCHAR(255) NULL,
+  totalprice VARCHAR (255) NULL,
+  token TEXT NULL,
+  FOREIGN KEY(vendorId) REFERENCES vendors(vendorId)
+);
+
+CREATE TABLE orderitems (
+  orderitemsId BIGINT NOT NULL AUTO_INCREMENT,
+  amount VARCHAR (255) NULL,
+  productdesc VARCHAR (255) NULL,
+  qty VARCHAR (255) NULL,
+  price VARCHAR (255) NULL,
+  totalrowprice VARCHAR (255) NULL,
+  date DATETIME NULL,
+  FOREIGN KEY(orderId) REFERENCES orders(orderId),
+  FOREIGN KEY(vendorId) REFERENCES vendors(vendorId)
 );
 
